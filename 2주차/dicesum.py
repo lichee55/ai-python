de5df35ccd539023c57a6179262c16ac7987a7bd
liche <lichee55@gmail.com>
2021-03-08 15:34:19 +0900

    [Add] dicesum.py
    두 주사위 합 예측

---
+++ 2주차/dicesum.py
@@ -0,0 +1,24 @@
+# 20210308 Mon
+# 인공지능 파이썬 수업 2주차
+
+import random
+
+x=random.randint(1,6)
+y=random.randint(1,6)
+
+# print(x, y)
+
+a=input('a 예측 : ')
+b=input('b 예측 : ')
+
+a=int(a)
+b=int(b)
+
+print('첫 주사위 : ',x,end=" ")
+print('두번째 주사위 : ',y,end=" ")
+print('합은 ',x+y)
+
+if(((x+y)-a)**2>((x+y)-b)**2):
+    print("b승")
+else:
+    print("a승")
